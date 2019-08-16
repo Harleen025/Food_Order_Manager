0907d95ede8f1994b3c1068a762fa6b80924652d
Harleen025 <54070671+Harleen025@users.noreply.github.com>
2019-08-16 00:31:12 -0700

    Update Food.java

--- app/src/main/java/com/example/hp/myapplication/Model/Food.java
+++ app/src/main/java/com/example/hp/myapplication/Model/Food.java
@@ -6,7 +6,7 @@ public class Food {
     public Food(){
 
     }
-
+// Getting Foods Details
     public Food(String name, String image, String description, String price, String discount, String menuId){
         Name = name;
         Image = image;
@@ -15,51 +15,51 @@ public class Food {
         Discount = discount;
         MenuId = menuId;
     }
-
+//Get name
     public String getName() {
         return Name;
     }
-
+// Set name
     public void setName(String name) {
         Name = name;
     }
-
+// Get Image
     public String getImage() {
         return Image;
     }
-
+//Set Image
     public void setImage(String image) {
         Image = image;
     }
-
+// Get Description
     public String getDescription() {
         return Description;
     }
-
+//Set Description
     public void setDescription(String description) {
         Description = description;
     }
-
+//Get Price
     public String getPrice() {
         return Price;
     }
-
+//Set Price
     public void setPrice(String price) {
         Price = price;
     }
-
+// Get Discount
     public String getDiscount() {
         return Discount;
     }
-
+//Set Discount
     public void setDiscount(String discount) {
         Discount = discount;
     }
-
+// Get Menu ID
     public String getMenuId() {
         return MenuId;
     }
-
+//Set Menu Id
     public void setMenuId(String menuId) {
         MenuId = menuId;
     }
